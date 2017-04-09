c3f5713df8513917c21b13138dff0f8e09b16176
Wasid Hossain <wasidmail@gmail.com>
2017-04-09 09:32:55 +0000

    asset implemented by including partials

--- expressmore/EJSdemo/app.js
+++ expressmore/EJSdemo/app.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/inlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingvar: thing});
+    res.render("love", {thingvar: thing});
 });
 
 
@@ -18,9 +20,12 @@ app.get("/posts", function(req, res){
         {title: "Can you believe this PHP?", author: "Hossain"}
     ];
     
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 })
 
+app.get("*", function(req, res){
+    res.send("Route not Available!!!")
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
