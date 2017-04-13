a329ff485e5e0cd5bd4565d5009dfa2389d3130c
Wasid Hossain <wasidmail@gmail.com>
2017-04-13 08:57:13 +0000

    Movie API app part 1 done

---
+++ APIs/movie_search_app/app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+var request = require('request');
+
+app.set("view engine", "ejs");
+
+
+app.get("/results", function(req, res){
+    request('http://www.omdbapi.com/?s=prison', function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      var persedData = JSON.parse(body);
+    //   console.log('body:', persedData['query']['results']['channel']['astronomy']['sunset']); // Print the HTML for the Google homepage.
+       // Print the HTML for the Google homepage.
+      if (!error && response.statusCode == 200) {
+          res.send(persedData.Search[2].Title);
+      }
+    });
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server Has Started For Movie App!!!")
+});
