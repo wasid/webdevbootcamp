52a734169cabbb5478a51f822d0b808510ede137
Wasid Hossain <wasidmail@gmail.com>
2017-04-14 05:51:07 +0000

    YelpCamp v1 with basic routes

---
+++ yelpcamp/app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    
+    var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+    ];
+    
+    res.render("campgrounds", {campgrounds: campgrounds});
+    
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("YelpCamp Server Has Started For Movie App!!!")
+});
