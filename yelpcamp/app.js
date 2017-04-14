9688c684a2aa0467244183f6bb297e1cd92423f5
Wasid Hossain <wasidmail@gmail.com>
2017-04-14 09:26:43 +0000

    add new campground with POST method

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+var campgrounds = [
+        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
+    ];
 
 
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -11,19 +18,24 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
     
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-    ];
+   res.render("campgrounds", {campgrounds: campgrounds});
     
-    res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCamp = {name:name, image:image}
+    console.log(newCamp);
+    campgrounds.push(newCamp);
+    res.redirect("/campgrounds");
+    
+});
+app.get("/campgrounds/new", function(req, res){
+    
+
+    res.render("new");
     
 });
 
@@ -31,5 +43,5 @@ app.get("/campgrounds", function(req, res){
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("YelpCamp Server Has Started For Movie App!!!")
+    console.log("YelpCamp Server Has Started!!!")
 });
