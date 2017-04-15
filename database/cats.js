d4175ec73e55146df9033a9bad43327566297dc1
Wasid Hossain <wasidmail@gmail.com>
2017-04-15 05:43:44 +0000

    refactor code to create and read

--- database/cats.js
+++ database/cats.js
@@ -10,18 +10,42 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-var jerry = new Cat({
-    name: "tom",
-    age: 5,
-    location: "CN"
-});
+// var jerry = new Cat({
+//     name: "tom",
+//     age: 5,
+//     location: "CN"
+// });
 
-jerry.save(function(err, cat){
+// jerry.save(function(err, cat){
+//     if (err) {
+//         console.log("Failed to save Cat to the DB!");
+//     } else {
+//         console.log("Cat saved to the DB!");
+//         console.log(jerry);
+//     }
+// });
+
+Cat.create({
+    name: "pussy",
+    age: 6,
+    location: "nick"
+}, function(err, newcat){
     if (err) {
         console.log("Failed to save Cat to the DB!");
     } else {
         console.log("Cat saved to the DB!");
-        console.log(jerry);
+        console.log(newcat);
     }
+});
+
+Cat.find({}, function(err, allcat){
+     if (err) {
+        console.log("Failed to fetch Cat data from the DB!");
+        console.log(err);
+    } else {
+        console.log("Cat info:");
+        console.log(allcat);
+    }
+    
 });
 
