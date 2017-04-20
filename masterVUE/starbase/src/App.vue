e721d1e7798a37f519f4b2d2e9f4f0b573542d9a
Wasid Hossain <wasidmail@gmail.com>
2017-04-20 18:54:02 +0000

    Introducing the API part done

--- masterVUE/starbase/src/App.vue
+++ masterVUE/starbase/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!--<img src="./assets/logo.png">-->
     <h1>{{title}}</h1>
+    <button class="btn btn-success" @click="fetchdata">get info</button>
   </div>
 </template>
 
@@ -11,6 +12,13 @@ export default {
   data () {
     return {
       title: 'Generate your team'
+    }
+  },
+  methods: {
+    fetchdata(){
+      fetch("http://swapi.co/api/people/1", {
+        method:"GET"
+      }).then(respose => respose.json()).then(json => console.log(json))
     }
   }
 }
