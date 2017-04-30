cce274f5dac8d18160560f4d610fcd642f370a1c
Wasid Hossain <wasidmail@gmail.com>
2017-04-30 05:48:38 +0000

    YelpCamp: Styling Comments Pt 1

--- yelpcamp/seed.js
+++ yelpcamp/seed.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        info: "blah blah blah"
+        info: "Bacon ipsum dolor amet sirloin beef ribs beef tongue, chuck corned beef burgdoggen jerky bresaola pork loin salami venison. Tail bresaola ham venison sirloin. Ball tip pastrami kielbasa, shank pork belly kevin flank boudin capicola jerky short loin brisket. Beef capicola frankfurter, beef ribs tail pork belly kielbasa spare ribs brisket bresaola jowl ball tip short ribs pork loin cupim. Meatloaf kielbasa pork belly, ribeye pork loin boudin brisket venison jowl. Ground round pork loin shank boudin chicken."
     },
     {
         name: "Desert Mesa", 
         image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg",
-        info: "blah blah blah"
+        info: "Bacon ipsum dolor amet sirloin beef ribs beef tongue, chuck corned beef burgdoggen jerky bresaola pork loin salami venison. Tail bresaola ham venison sirloin. Ball tip pastrami kielbasa, shank pork belly kevin flank boudin capicola jerky short loin brisket. Beef capicola frankfurter, beef ribs tail pork belly kielbasa spare ribs brisket bresaola jowl ball tip short ribs pork loin cupim. Meatloaf kielbasa pork belly, ribeye pork loin boudin brisket venison jowl. Ground round pork loin shank boudin chicken."
     },
     {
         name: "Canyon Floor", 
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        info: "blah blah blah"
+        info: "Bacon ipsum dolor amet sirloin beef ribs beef tongue, chuck corned beef burgdoggen jerky bresaola pork loin salami venison. Tail bresaola ham venison sirloin. Ball tip pastrami kielbasa, shank pork belly kevin flank boudin capicola jerky short loin brisket. Beef capicola frankfurter, beef ribs tail pork belly kielbasa spare ribs brisket bresaola jowl ball tip short ribs pork loin cupim. Meatloaf kielbasa pork belly, ribeye pork loin boudin brisket venison jowl. Ground round pork loin shank boudin chicken."
     }
     ];
 
