277f48f5402d87ee2be1599a10ffb8790bc12c34
Wasid Hossain <wasidmail@gmail.com>
2017-04-30 11:12:06 +0000

    Secret Page Code Along Pt. 2 config passport with User model

--- auth/authdemo/app.js
+++ auth/authdemo/app.js
@@ -1,12 +1,29 @@
-var express = require("express");
-
-// database connection
-var mongoose = require("mongoose");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    passportLocal         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 
 app.set("view engine", "ejs");
+
+app.use(require("express-session")({
+   secret: "Ocean can be a great programmer.",
+   resave: false,
+   saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
