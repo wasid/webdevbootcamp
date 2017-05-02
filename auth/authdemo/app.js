d3177ddd9e0a576f6355d56003821b24a28c1851
Wasid Hossain <wasidmail@gmail.com>
2017-05-02 06:11:46 +0000

    Secret Page Code Along Pt. 3 using passport-local-mongoose

--- auth/authdemo/app.js
+++ auth/authdemo/app.js
@@ -11,6 +11,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(require("express-session")({
    secret: "Ocean can be a great programmer.",
@@ -25,6 +26,7 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Routes
 
 app.get("/", function(req, res){
     res.render("home");
@@ -34,6 +36,28 @@ app.get("/secret", function(req, res){
     res.render("secret");
 });
 
+// Auth Routes
+
+// Sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Handle Sign up
+app.post("/register", function(req, res){
+    var username = req.body.username
+    var password = req.body.password
+    User.register(new User({username: username}), password, function(err, user){
+        if (err) {
+            console.log(err);
+            return res.render("/register");
+        } else {
+            passport.authenticate("local")(req, res,  function(){
+                res.redirect("/secret");
+            })
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Auth App Server Has Started!!!")
